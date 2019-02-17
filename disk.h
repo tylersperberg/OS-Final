255ffc34c949f51565d778dea71fc532a0c83cfb
Jordan <jordanhasty@yahoo.com>
2019-02-16 19:27:01 -0500

    Update disk.h

--- disk.h
+++ disk.h
@@ -7,7 +7,7 @@ class Disk
 {
 private:
 	// memory size in Bytes (4 Bytes in a word)
-	static const unsigned int diskSize = 2048 * 4; // 2048 words
+	static const unsigned int DISK_SIZE = 2048 * 4; // 2048 words
 	
 public:
 	Disk();
