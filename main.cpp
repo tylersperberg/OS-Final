bff15b46f9a4e3110a5ed63c61c5ebaa0cc45013
plore-0 <jordanhasty@yahoo.com>
2019-02-26 20:43:36 -0500

    added memory read/write functions

--- main.cpp
+++ main.cpp
@@ -2,11 +2,28 @@
 
 int main()
 {
-	//std::cout << "test" << std::endl;
- //   initialize disk with size 2048
- //   initialize ram with size 1024
- //   create cpus
- //   create scheduler
- //   call driver to load the file
-    
+	 //   initialize disk with size 2048
+	 //   initialize ram with size 1024
+	 //   create cpus
+	 //   create scheduler
+	 //   call driver to load the file
+	
+	/**DEBUGGING**/
+	
+	// memory test
+	
+	// write
+	int x = -1234;
+	Memory::writeMemory(0, &x, sizeof(x));
+	
+	// show memory
+	std::cout << "first 32 bytes of memory:" << std::endl;
+	Memory::printMemBlock(0, 32);
+	
+	// read
+	int y;
+	Memory::readMemory(0, &y, sizeof(y));
+	
+	// check if it read and wrote properly
+	std::cout << std::dec << "value of y: " << y << std::endl;
 }
--- memory.h
+++ memory.h
@@ -1,15 +1,16 @@
+// Memory is byte-accessible
+
 #ifndef MEMORY_H
 #define MEMORY_H
 
 #include "globals.h"
 #include <cstdlib>
-// Memory indexes are per byte.
 
 class Memory
 {
 private:
-	// memory size in bits
-	static const unsigned int MEMORY_SIZE = 1024 * 4; // 1024 words * 4 bytes
+	// memory size in Bytes
+	static const unsigned int MEMORY_SIZE = 1024 * 4; // 1024 Words * 4 Bytes
 
 	// memory contents
 	static types::Byte data[MEMORY_SIZE];
@@ -18,12 +19,16 @@ public:
 	// reads memory and stores it in the buffer
 	// needs the base address, pointer to the buffer, and number of bytes to be read ( must not exceed sizeof(buffer) )
 	template<typename T>
-	static void readMemory(int baseAddress, T* buffer, size_t dataSize);
+	static void readMemory(unsigned int baseAddress, T* buffer, size_t dataSize);
 	
 	// writes memory at the specified base address with the data given in the buffer
 	// needs the base address, pointer to the buffer, and number of bytes to be written ( must not exceed sizeof(buffer) )
 	template<typename T>
-	static void writeMemory(int baseAddress, T* buffer, size_t dataSize);
+	static void writeMemory(unsigned int baseAddress, T* buffer, size_t dataSize);
+	
+	/**DEBUG FUNCTIONS**/
+	// print the contents of a memory block given a base address and size of block
+	static void printMemBlock(unsigned int baseAddress, size_t size);
 };
 
 #include "memory.template"
