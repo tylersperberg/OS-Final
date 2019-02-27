cdc39719dcc23ca294e6a962402cee1ff885f4f4
plore-0 <jordanhasty@yahoo.com>
2019-02-26 21:07:52 -0500

    changed memory from static to instantiated

--- memory.cpp
+++ memory.cpp
@@ -4,7 +4,15 @@
 #include <iostream>
 
 // initialize memory contents
-types::Byte Memory::data[MEMORY_SIZE];
+Memory::Memory()
+{
+	types::Byte data[MEMORY_SIZE];
+}
+
+Memory::~Memory()
+{
+	types::Byte data[MEMORY_SIZE];
+}
 
 //**DEBUG FUNCTIONS**//
 void Memory::printMemBlock(unsigned int baseAddress, size_t size)
--- memory.h
+++ memory.h
@@ -13,22 +13,25 @@ private:
 	static const unsigned int MEMORY_SIZE = 1024 * 4; // 1024 Words * 4 Bytes
 
 	// memory contents
-	static types::Byte data[MEMORY_SIZE];
+	types::Byte data[MEMORY_SIZE];
 	
 public:
+	Memory();
+	~Memory();
+
 	// reads memory and stores it in the buffer
 	// needs the base address, pointer to the buffer, and number of bytes to be read ( must not exceed sizeof(buffer) )
 	template<typename T>
-	static void readMemory(unsigned int baseAddress, T* buffer, size_t dataSize);
+	void readMemory(unsigned int baseAddress, T* buffer, size_t dataSize);
 	
 	// writes memory at the specified base address with the data given in the buffer
 	// needs the base address, pointer to the buffer, and number of bytes to be written ( must not exceed sizeof(buffer) )
 	template<typename T>
-	static void writeMemory(unsigned int baseAddress, T* buffer, size_t dataSize);
+	void writeMemory(unsigned int baseAddress, T* buffer, size_t dataSize);
 	
 	/**DEBUG FUNCTIONS**/
 	// print the contents of a memory block given a base address and size of block
-	static void printMemBlock(unsigned int baseAddress, size_t size);
+	void printMemBlock(unsigned int baseAddress, size_t size);
 };
 
 #include "memory.template"
