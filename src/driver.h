f681b5ac60ae56afe01c779340ff6e0dad1d2edb
plore-0 <jordanhasty@yahoo.com>
2019-03-01 20:30:05 -0500

    some basics for PCB and jobs vector (job queue?)

--- src/driver.h
+++ src/driver.h
@@ -1,10 +1,22 @@
 #ifndef DRIVER_H
 #define DRIVER_H
 
+#include <vector>
+
 class Driver
 {
 private:
-	void loader(); // should the loader be a function?
+	struct PCB
+	{
+		/*
+		 * number of instructions
+		 * base address
+		 * idk
+		 */
+	};
+	
+	std::vector<PCB> jobs;
+	
 	
 public:
 	Driver();
